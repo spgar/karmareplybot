afff8bfd883c6bd4ff78bee286eca588a15752c7
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-01-15 12:57:11 -0500

    Add minimum comment/karma requirement.

---
+++ replybot.py
@@ -0,0 +1,122 @@
+#/u/GoldenSights
+import traceback
+import praw # simple interface to the reddit API, also handles rate limiting of requests
+import time
+import sqlite3
+
+'''USER CONFIGURATION'''
+
+APP_ID = ""
+APP_SECRET = ""
+APP_URI = ""
+APP_REFRESH = ""
+# https://www.reddit.com/comments/3cm1p8/how_to_make_your_bot_use_oauth2/
+USERAGENT = ""
+# This is a short description of what the bot does.
+# For example "Python automatic replybot v2.0 (by /u/GoldenSights)"
+SUBREDDIT = "pics"
+# This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subreddits, use "sub1+sub2+sub3+..."
+KEYWORDS = ["phrase 1", "phrase 2", "phrase 3", "phrase 4"]
+# These are the words you are looking for
+KEYAUTHORS = []
+# These are the names of the authors you are looking for
+# The bot will only reply to authors on this list
+# Keep it empty to allow anybody.
+REPLYSTRING = "Hi hungry, I'm dad."
+# This is the word you want to put in reply
+MAXPOSTS = 100
+# This is how many posts you want to retrieve all at once. PRAW can download 100 at a time.
+WAIT = 30
+# This is how many seconds you will wait between cycles. The bot is completely inactive during this time.
+MINLINKKARMA = 0
+# Minimum link karma author must have to trigger reply
+MINCOMMENTKARMA = 0
+# Minimum comment karma author must have to trigger reply
+
+CLEANCYCLES = 10
+# After this many cycles, the bot will clean its database
+# Keeping only the latest (2*MAXPOSTS) items
+
+'''All done!'''
+
+try:
+    import bot
+    USERAGENT = bot.aG
+except ImportError:
+    pass
+
+print('Opening SQL Database')
+sql = sqlite3.connect('sql.db')
+cur = sql.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(id TEXT)')
+
+print('Logging in...')
+r = praw.Reddit(USERAGENT)
+r.set_oauth_app_info(APP_ID, APP_SECRET, APP_URI)
+r.refresh_access_information(APP_REFRESH)
+
+def replybot():
+    print('Searching %s.' % SUBREDDIT)
+    subreddit = r.get_subreddit(SUBREDDIT)
+    posts = list(subreddit.get_comments(limit=MAXPOSTS))
+    posts.reverse()
+    for post in posts:
+        # Anything that needs to happen every loop goes here.
+        pid = post.id
+
+        try:
+            pauthor = post.author.name
+        except AttributeError:
+            # Author is deleted. We don't care about this post.
+            continue
+
+        if pauthor.lower() == r.user.name.lower():
+            # Don't reply to yourself, robot!
+            print('Will not reply to myself.')
+            continue
+
+        if KEYAUTHORS != [] and all(auth.lower() != pauthor for auth in KEYAUTHORS):
+            # This post was not made by a keyauthor
+            continue
+
+        redditor = r.get_redditor(pauthor)
+        if redditor.link_karma < MINLINKKARMA:
+            # Author does not meet minimum link karma requirement
+            continue
+
+        if redditor.comment_karma < MINCOMMENTKARMA:
+            # Author does not meet minimum comment karma requirement
+            continue
+
+        cur.execute('SELECT * FROM oldposts WHERE ID=?', [pid])
+        if cur.fetchone():
+            # Post is already in the database
+            continue
+
+        pbody = post.body.lower()
+        if not any(key.lower() in pbody for key in KEYWORDS):
+            # Does not contain our keyword
+            continue
+
+        cur.execute('INSERT INTO oldposts VALUES(?)', [pid])
+        sql.commit()
+        print('Replying to %s by %s' % (pid, pauthor))
+        try:
+            post.reply(REPLYSTRING)
+        except praw.errors.Forbidden:
+            print('403 FORBIDDEN - is the bot banned from %s?' % post.subreddit.display_name)
+
+cycles = 0
+while True:
+    try:
+        replybot()
+        cycles += 1
+    except Exception as e:
+        traceback.print_exc()
+    if cycles >= CLEANCYCLES:
+        print('Cleaning database')
+        cur.execute('DELETE FROM oldposts WHERE id NOT IN (SELECT id FROM oldposts ORDER BY id DESC LIMIT ?)', [MAXPOSTS * 2])
+        sql.commit()
+        cycles = 0
+    print('Running again in %d seconds \n' % WAIT)
+    time.sleep(WAIT)
